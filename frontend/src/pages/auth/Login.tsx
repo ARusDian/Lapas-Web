de21bad1f51a0b75a00ad91c17ca12cf98b61d3e
dimpram101 <dimas2004211@gmail.com>
2023-10-26 00:08:06 +0800

    fix(FE): fix login design

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -15,25 +15,26 @@ const Login = () => {
   }
 
   return (
-    <div className='flex flex-col justify-center h-[100vh]'>
-      <div className="w-1/3 h-96 mx-auto bg-white drop-shadow-lg border rounded-2xl p-4">
-        <form onSubmit={handleSubmit} className='flex flex-col justify-center gap-4 h-full'>
-          <h1 className='text-3xl font-bold text-center'>LapasPanic</h1>
+    <div className='flex justify-between items-center h-[100vh] gap-8'>
+      <div className="h-full w-full">
+        <img src="/assets/lapas-bg-main.png" className='w-full h-full bg-cover brightness-50' />
+      </div>
+      <div className="w-[800px] h-96 mx-auto p-4 mr-8">
+        <h1 className='text-3xl font-bold text-center'>LapasPanic</h1>
+        <form onSubmit={handleSubmit}  className='flex flex-col h-full gap-4 mt-4'>
           <div className='flex flex-col gap-1'>
-            <label htmlFor="email">Email</label>
-            <TextField type="email" name="email" id="email" value={form.email} onChange={(e) => setForm(prev => ({ ...prev, email: e.target.value }))} />
+            <label htmlFor="email">Email*</label>
+            <TextField required id="email" variant="outlined" size='medium'  value={form.email} onChange={(e) => setForm(prev => ({ ...prev, email: e.target.value }))} />
           </div>
           <div className='flex flex-col gap-1'>
-            <label htmlFor="password">Password</label>
-            <TextField type="password" name="password" id="password" value={form.password} onChange={(e) => setForm(prev => ({ ...prev, password: e.target.value }))} />
+          <label htmlFor="password">Password*</label>
+            <TextField required id="password" type='password' variant="outlined" size='medium'  value={form.password} onChange={(e) => setForm(prev => ({ ...prev, password: e.target.value }))} />
           </div>
           <Button type='submit' variant='contained' size='large' endIcon={<LoginIcon />}>Login</Button>
         </form>
       </div>
-
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" className='fixed -bottom-16 left-0 -z-10'><path fill="#0099ff" fillOpacity="1" d="M0,128L34.3,138.7C68.6,149,137,171,206,181.3C274.3,192,343,192,411,208C480,224,549,256,617,240C685.7,224,754,160,823,160C891.4,160,960,224,1029,250.7C1097.1,277,1166,267,1234,256C1302.9,245,1371,235,1406,229.3L1440,224L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path></svg>
     </div>
   )
 }
 
-export default Login;
+export default Login;
