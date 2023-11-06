81fdf7780db9de57f1fa6e1a799f5cfa3f27356b
RUVNE <dian.galleon@gmail.com>
2023-11-06 21:53:03 +0800

    fix(BE): notification missing user Id AND type

--- backend/src/services/NotificationService.ts
+++ backend/src/services/NotificationService.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { PushButtonLogCreationModel, DeviceTokenModel, UserAuthInfoRequest } from "../models";
+import { PushButtonLogCreationModel, DeviceTokenModel, UserAuthInfoRequest, ErrorResponse, ErrorDetails } from "../models";
 import { DeviceTokenValidation } from "./validations/DeviceToken";
 import admin from "firebase-admin";
 
@@ -8,20 +8,55 @@ const prisma = new PrismaClient();
 
 export const postDeviceTokenService = async (req: UserAuthInfoRequest): Promise<string> => {
     const { token } = DeviceTokenValidation(req.body);
+
+    if (req.userId === undefined) {
+        throw new ErrorResponse(
+            401,
+            "Unauthorized",
+            new ErrorDetails(
+                "DeviceTokenErrorSave",
+                "DeviceTokenError",
+                "User not found, please login to continue"
+            )
+        );
+    }
     
     const deviceToken = prisma.deviceToken.create({
         data: {
             deviceToken: token,
-            // TODO: FIX LATER
-            userId: 1
+            userId: req.userId
         }
     });
-    // @ts-ignore
-    return deviceToken;
+
+    return "Device token saved successfully";
 }
 
 export const postNotificationService = async (req: UserAuthInfoRequest) => {
     const { type, userId } = req.body;
+
+    if (!type) {
+        throw new ErrorResponse(
+            400,
+            "Bad Request",
+            new ErrorDetails(
+                "NotificationError",
+                "NotificationError",
+                "Type is required"
+            )
+        );
+    }
+
+    if (!userId) {
+        throw new ErrorResponse(
+            400,
+            "Bad Request",
+            new ErrorDetails(
+                "NotificationError",
+                "NotificationError",
+                "User not found, please login to continue"
+            )
+        );
+    }
 
     const deviceTokens = await prisma.deviceToken.findMany({
         select: {
@@ -33,14 +68,11 @@ export const postNotificationService = async (req: UserAuthInfoRequest) => {
 
     const payload = {
         notification: {
-            title: "KEBAKARAN",
-            body: "Pak Bowo Keren"
+            title: type,
+            body: "Button pushed"
         },
         tokens: registrationTokens
     }
-
-    console.log(registrationTokens);
-
 
     const response = await admin.messaging().sendMulticast(payload)
 
