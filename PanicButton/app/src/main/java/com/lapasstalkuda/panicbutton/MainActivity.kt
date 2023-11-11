7f48073ae0df68dabf6ff9411c4777e0134b5292
Ervalsa Dwi Nanda <ervalsananda@gmail.com>
2023-11-11 13:08:38 +0700

    redesain halaman home

--- PanicButton/app/src/main/java/com/lapasstalkuda/panicbutton/MainActivity.kt
+++ PanicButton/app/src/main/java/com/lapasstalkuda/panicbutton/MainActivity.kt
@@ -3,29 +3,37 @@ package com.lapasstalkuda.panicbutton
 import android.Manifest
 import android.os.Build
 import android.os.Bundle
+import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.lapasstalkuda.panicbutton.databinding.ActivityMainBinding
+import me.ibrahimsn.lib.SmoothBottomBar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        navController = findNavController(R.id.nav_host_fragment_activity_main)
+        setupSmoothBottomMenu()
+    }
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        navView.setupWithNavController(navController)
+    private fun setupSmoothBottomMenu() {
+        val popUpMenu = PopupMenu(this, null)
+        popUpMenu.inflate(R.menu.bottom_nav_menu)
+        val menu = popUpMenu.menu
+        binding.bottomBar.setupWithNavController(menu, navController)
     }
 }
